b17f8026ea9533d4a81ee5b9cbe2d34c78ddfbc6
Peter <peter@google.com>
2023-10-26 09:56:50 +0800

    增加游戏分类属性

---
+++ BetWin.Game.API/Enums/GameCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BetWin.Game.API.Enums
+{
+    public enum GameCategory : byte
+    {
+        [Description("彩票")]
+        Lottery = 1,
+        [Description("电竞")]
+        ESport = 2,
+        [Description("电子")]
+        Slot = 3,
+        [Description("真人")]
+        Live = 4,
+        [Description("棋牌")]
+        Chess = 5,
+        [Description("体育")]
+        Sport = 6,
+        [Description("捕鱼")]
+        Fish = 7,
+    }
+}
--- BetWin.Game.API/Enums/GameType.cs
+++ BetWin.Game.API/Enums/GameType.cs
@@ -7,15 +7,29 @@ namespace BetWin.Game.API.Enums
 {
     public enum GameType : byte
     {
-        [Description("EVO真人")]
+        [Description("EVO真人"), Game(Category = GameCategory.Live)]
         EVOLive = 1,
-        [Description("SA真人")]
+        [Description("SA真人"), Game(Category = GameCategory.Live)]
         SALive,
+        [Description("OB真人"), Game(Category = GameCategory.Live)]
+        OBLive,
 
-        [Description("VR彩票")]
+        [Description("VR彩票"), Game(Category = GameCategory.Lottery)]
         VRLottery = 20,
+        [Description("OB彩票"), Game(Category = GameCategory.Lottery)]
+        OBLottery = 21,
 
-        [Description("OB电竞")]
+        [Description("OB电竞"), Game(Category = GameCategory.ESport)]
         OBESport = 40,
+        [Description("雷火电竞"), Game(Category = GameCategory.ESport)]
+        TFESport = 41,
+
+        [Description("开元棋牌"), Game(Category = GameCategory.Chess)]
+        KYChess = 60
+    }
+
+    public class GameAttribute : Attribute
+    {
+        public GameCategory Category { get; set; }
     }
 }
